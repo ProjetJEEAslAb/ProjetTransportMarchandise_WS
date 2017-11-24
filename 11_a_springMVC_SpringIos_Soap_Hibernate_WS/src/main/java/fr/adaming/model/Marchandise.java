55275f87423d47a84432a73778c497d9e9822081
inti0455 <inti0455@192.168.1.181>
2017-11-24 10:02:20 +0100

    Denis : commit marchandise

--- 11_a_springMVC_SpringIos_Soap_Hibernate_WS/src/main/java/fr/adaming/model/Marchandise.java
+++ 11_a_springMVC_SpringIos_Soap_Hibernate_WS/src/main/java/fr/adaming/model/Marchandise.java
@@ -3,6 +3,9 @@ package fr.adaming.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -12,6 +15,8 @@ import javax.persistence.Table;
 public class Marchandise implements Serializable {
 
 	// ==============attributs================//
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String nom;
 	private double poids;
@@ -80,6 +85,22 @@ public class Marchandise implements Serializable {
 		this.volume = volume;
 	}
 
+	public CargaisonRoutiere getCargaisonRoutiere() {
+		return cargaisonRoutiere;
+	}
+
+	public void setCargaisonRoutiere(CargaisonRoutiere cargaisonRoutiere) {
+		this.cargaisonRoutiere = cargaisonRoutiere;
+	}
+
+	public CargaisonAerienne getCargaisonAerienne() {
+		return cargaisonAerienne;
+	}
+
+	public void setCargaisonAerienne(CargaisonAerienne cargaisonAerienne) {
+		this.cargaisonAerienne = cargaisonAerienne;
+	}
+	
 	// ===============methode tostring===================//
 	
 	@Override
